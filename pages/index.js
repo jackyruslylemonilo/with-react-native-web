65becd51efee36867abb64e23d3db95b33f3cd82
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-02-05 14:08:51 +0700

    Image

--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image
+      <img src="https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75" />
+      {/* <Image
         source={{ uri: 'https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75 '}}
         style={{ width: 500, height: 500 }}
-      />
+      /> */}
       
       <Header />
       <Text accessibilityRole="header" style={styles.text}>
