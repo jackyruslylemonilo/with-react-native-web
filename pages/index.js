770edb93c21625a198c9e7204fecdb41a59ec614
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-02-15 11:27:59 +0700

    Image

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* <img src="https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75" style={{ width: '500px', height: '500px' }} /> */}
-      <Image
+      <img src="https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75" style={{ width: '500px', height: '500px' }} />
+      {/* <Image
         source={{ uri: 'https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75 '}}
         style={{ width: 500, height: 500 }}
       />
-      
+       */}
       <Header />
       <Text accessibilityRole="header" style={styles.text}>
         React Native for Web & Next.js
