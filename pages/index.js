efbee4c6fc904f8ce68e5fc19e264d97517dcd6e
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-07-27 18:01:35 +0700

    Test

--- pages/index.js
+++ pages/index.js
@@ -33,11 +33,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* <img src="https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75" style={{ width: '500px', height: '500px' }} /> */}
-      <Image
+      <img src="https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75" style={{ width: '500px', height: '500px' }} />
+      {/* <Image
         source={{ uri: 'https://ik.imagekit.io/10tn5i0v1n/dynamic-pages/wiranilo/Desktop-1-Banner-Header.jpg?tr=w-800,q-75 ' }}
         style={{ width: 500, height: 500 }}
-      />
+      /> */}
 
       <Link href="/alternate">
         <button onClick={() => console.log('Clicked 1')}>
