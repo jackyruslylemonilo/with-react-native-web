439936cd8bf24068f0d48ecd939e8212427a9871
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-09-29 10:51:40 +0700

    Testing

---
+++ pages/app-version.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from 'react';
+
+const transformAppVersionToNumber = (version) => {
+  const appVersion = version.toString().split('.');
+
+  const baseMultipler = 100;
+  let multiplier = Math.pow(baseMultipler, appVersion.length);
+  let versionInNumber = 0;
+
+  appVersion.forEach((item) => {
+    versionInNumber += parseInt(item) * multiplier;
+    multiplier /= baseMultipler;
+  });
+
+  return versionInNumber;
+};
+
+const AnniversaryGamePage = ({ userAgent }) => {
+  useEffect(() => {
+    async function init() {
+      if (userAgent.indexOf('lemonilo/') !== 0) {
+        window.location.replace('/');
+      } else {
+        const [firstSegmentUserAgent] = userAgent.split(';');
+        const [
+          _appName,
+          platform,
+          appBuildVersion,
+        ] = firstSegmentUserAgent.split('/');
+        const [_appBuildNumber, appVersion] = appBuildVersion?.split(' - ');
+
+        const minVersion = transformAppVersionToNumber('1.15.0');
+        const currentVersion = transformAppVersionToNumber(appVersion);
+
+        if (minVersion < currentVersion) {
+          if (platform === 'android') {
+            window.open(
+              'https://play.google.com/store/apps/details?id=com.lemonilo',
+            );
+          } else {
+            window.open(
+              'https://itunes.apple.com/us/app/lemonilo/id1450623533',
+            );
+          }
+        } else {
+          window.open('lemonilo://cart');
+        }
+
+        console.log(minVersion, currentVersion);
+      }
+    }
+
+    init();
+  }, [userAgent]);
+
+  return <div />;
+};
+
+AnniversaryGamePage.getInitialProps = async (ctx) => {
+  let userAgent = '';
+
+  if (ctx.req) {
+    userAgent = ctx.req.headers['user-agent'] ?? '';
+
+    if (ctx.res) {
+      ctx.res.statusCode = 404;
+    }
+  }
+
+  return {
+    userAgent,
+  };
+};
+
+export default AnniversaryGamePage;
