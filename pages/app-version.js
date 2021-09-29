0322503e0584699441220bfac2b7f5fb123d61cb
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-09-29 12:44:46 +0700

    Test

--- pages/app-version.js
+++ pages/app-version.js
@@ -39,10 +39,10 @@ const AnniversaryGamePage = ({ userAgent }) => {
 
         if (currentVersion < minVersionApp) {
           //'lemonilo://webview?url=https://www.lemonilo.com/p/wiranilo&replace_navigation=1'
-          window.location.replace(`${urlPrevVersion}&replace_navigation=1`)
+          window.location.replace(`${urlPrevVersion}${urlPrevVersion.includes('?') ? '&' : '?'}replace_navigation=1`)
         } else {
           //'lemonilo://landing-page-anniv-game?replace_navigation=1'
-          window.location.replace(`${urlCurrentVersion}?replace_navigation=1`);
+          window.location.replace(`${urlCurrentVersion}${urlPrevVersion.includes('?') ? '&' : '?'}replace_navigation=1`);
         }
       }
     }
