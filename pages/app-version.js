0636365d18f96fe1c69156659bda40ac26711d7c
Jacky Rusly <jacky.rusly@lemonilo.com>
2021-09-29 11:16:03 +0700

    Test

--- pages/app-version.js
+++ pages/app-version.js
@@ -18,49 +18,16 @@ const transformAppVersionToNumber = (version) => {
 const AnniversaryGamePage = ({ userAgent }) => {
   useEffect(() => {
     async function init() {
-      if (userAgent.indexOf('lemonilo/') !== 0) {
-        window.location.replace('/');
-      } else {
-        setTimeout(() => {
-          window.open('lemonilo://cart');
+      setTimeout(() => {
+        window.open('lemonilo://cart');
 
-        }, 1000);
-        // const [firstSegmentUserAgent] = userAgent.split(';');
-        // const [
-        //   _appName,
-        //   platform,
-        //   appBuildVersion,
-        // ] = firstSegmentUserAgent.split('/');
-        // const [_appBuildNumber, appVersion] = appBuildVersion?.split(' - ');
-
-        // const minVersion = transformAppVersionToNumber('1.15.0');
-        // const currentVersion = transformAppVersionToNumber(appVersion.replace('v', ''));
-
-        // if (currentVersion < minVersion) {
-        //   if (platform === 'android') {
-        //     window.open('lemonilo://cart');
-
-        //     // window.location.href =
-        //     //   'https://play.google.com/store/apps/details?id=com.lemonilo'
-            
-        //   } else {
-        //     // window.location.href = 
-        //     //   'https://itunes.apple.com/us/app/lemonilo/id1450623533'
-        //     //   window.open('lemonilo://cart');
-        //     window.open('lemonilo://cart');
-        //   }
-        // } else {
-        //   window.open('lemonilo://cart');
-        // }
-
-        // console.log(minVersion, currentVersion);
-      }
+      }, 1000);
     }
 
     init();
   }, [userAgent]);
 
-  return <div>{userAgent} v1.0.1
+  return <div>{userAgent} v1.0.2
   
   <button onClick={() => {
     window.open('lemonilo://cart');
